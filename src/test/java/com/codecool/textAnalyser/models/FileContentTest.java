c9b7fb249e4a01851da84a1f227258ec9a383bd4
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-05-30 22:19:07 +0200

    Add maven, test getFileName

---
+++ src/test/java/com/codecool/textAnalyser/models/FileContentTest.java
@@ -0,0 +1,17 @@
+package com.codecool.textAnalyser.models;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileContentTest {
+
+    @Test
+    void getFileNameTest() {
+        String fileName = "test.txt";
+        FileContent fileContent = new FileContent(fileName);
+        assertEquals("test.txt", fileContent.getFileName());
+    }
+
+}
