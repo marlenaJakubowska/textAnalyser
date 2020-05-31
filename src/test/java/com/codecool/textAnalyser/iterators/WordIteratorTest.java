505e7bd53e03d59d01778a357da53e90ff99057f
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-05-31 14:48:58 +0200

    Refactor wordIterator tests

--- src/test/java/com/codecool/textAnalyser/iterators/WordIteratorTest.java
+++ src/test/java/com/codecool/textAnalyser/iterators/WordIteratorTest.java
@@ -3,6 +3,7 @@ package com.codecool.textAnalyser.iterators;
 import com.codecool.textAnalyser.controller.Controller;
 import com.codecool.textAnalyser.models.FileContent;
 import com.codecool.textAnalyser.view.View;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -16,8 +17,8 @@ class WordIteratorTest {
     private static OutputStream outputStream;
     private WordIterator wordIterator;
 
-    @Test
-    void checkCorrectNumberOfWords() {
+    @BeforeEach
+    void initializeComponents() {
         outputStream = new ByteArrayOutputStream();
         String[] args = {"test.txt"};
         FileContent fileContent = new FileContent(args[0]);
@@ -26,19 +27,15 @@ class WordIteratorTest {
         Controller ctrl = new Controller(args, view);
         ctrl.startAnalysis();
         wordIterator = new WordIterator(fileContent);
+    }
+
+    @Test
+    void checkCorrectNumberOfWords() {
         assertEquals(13, wordIterator.getWords().size());
     }
 
     @Test
     void checkIncorrectNumberOfWords() {
-        outputStream = new ByteArrayOutputStream();
-        String[] args = {"test.txt"};
-        FileContent fileContent = new FileContent(args[0]);
-        PrintStream ps = new PrintStream(outputStream);
-        View view = new View(ps);
-        Controller ctrl = new Controller(args, view);
-        ctrl.startAnalysis();
-        wordIterator = new WordIterator(fileContent);
         assertNotEquals(14, wordIterator.getWords().size());
     }
 }
